dde819842fd5f72fb6e87692e71a41e64c05e344
ihumentsev <a.ihumentsev@gmail.com>
2022-09-20 20:54:04 +0200

    add-newFitch

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,12 +7,7 @@ import css from '../components/App.module.css';
 
 class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
 
@@ -55,6 +50,17 @@ class App extends Component {
       };
     });
   };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts)
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+  }
+
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsContacts = JSON.parse(contacts);
+    if (parsContacts) this.setState({ contacts: parsContacts });
+  }
 
   render() {
     const { filter } = this.state;
